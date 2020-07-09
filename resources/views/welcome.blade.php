39e2c9202a1f2170633d99f5250a55280086d42f
yorunegy <yorunegy@gmail.com>
2020-07-09 13:50:54 +0000

    bootstrap導入

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,83 +4,36 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>laravel</title>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- fonts -->
-        <link href="https://fonts.googleapis.com/css?family=nunito:200,600" rel="stylesheet">
+        <title>上海プログラミング教室LINK</title>
 
-        <!-- styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
-            .full-height {
-                height: 100vh;
-            }
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">docs</a>
-                    <a href="https://laracasts.com">laracasts</a>
-                    <a href="https://laravel-news.com">news</a>
-                    <a href="https://blog.laravel.com">blog</a>
-                    <a href="https://nova.laravel.com">nova</a>
-                    <a href="https://forge.laravel.com">forge</a>
-                    <a href="https://vapor.laravel.com">vapor</a>
-                    <a href="https://github.com/laravel/laravel">github</a>
-                </div>
+        <header>
+            <div class="header-logo">
+                <img src="logo.webp">
             </div>
-        </div>
+            <div class="header-menus">
+                <li>ホーム</li>
+                <li>コース</li>
+                <li>開催日時</li>
+                <li>講師</li>
+                <li>レッスン料</li>
+                <li>よくある質問</li>
+            </div>
+        </header>
+        <main></main>
+        <footer></footer>
     </body>
 </html>
